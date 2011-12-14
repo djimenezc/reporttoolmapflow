79baf18a1ff5cc74af5bb4519f7def469af1f15b
david.jimenez19 <david.jimenez19@gmail.com>
2011-12-14 15:27:05 +0000

    Merge user bean from geouw database with the appfuse user
    Signed-off-by: david.jimenez19 <david.jimenez19@gmail.com>

--- core/src/main/java/com/mapflow/geo/common/model/User.java
+++ core/src/main/java/com/mapflow/geo/common/model/User.java
@@ -3,6 +3,7 @@ package com.mapflow.geo.common.model;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -11,7 +12,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.persistence.Transient;
+import javax.persistence.UniqueConstraint;
 import javax.persistence.Version;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
@@ -33,7 +37,7 @@ import org.springframework.security.core.userdetails.UserDetails;
  *         david@carter.net
  */
 @Entity
-@Table(name = "users")
+@Table(name = "USERS", schema = "GEOUW", uniqueConstraints = @UniqueConstraint(columnNames = "USERNAME"))
 @Searchable
 @XmlRootElement
 public class User extends BaseObject implements Serializable, UserDetails {
@@ -58,6 +62,19 @@ public class User extends BaseObject implements Serializable, UserDetails {
   private Address address;
   private String roles;
 
+  private String clientId;
+  private String comments;
+  private Long dataId;
+  private Date passwordExpiry;
+  private Long userRoleId;
+  private Date disabledDate;
+  private Date lastLoginDate;
+  private Date createdDate;
+  private String roleName;
+  private String emailAddress;
+  private String pwSalt;
+  private String branchName;
+
   /**
    * Default constructor - creates a new instance with no values set.
    */
@@ -74,25 +91,53 @@ public class User extends BaseObject implements Serializable, UserDetails {
     this.username = username;
   }
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  @SearchableId
-  public Long getId() {
-    return id;
+  public void addRole(final Role role) {
+
+    roles += role.getName();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof User)) {
+      return false;
+    }
+
+    final User user = (User) o;
+
+    return !(username != null ? !username.equals(user.getUsername()) : user.getUsername() != null);
+
+  }
+
+  public Address getAddress() {
+    return address;
   }
 
   @Override
-  @Column(nullable = false, length = 50, unique = true)
-  @SearchableProperty
-  public String getUsername() {
-    return username;
+  @Transient
+  public Collection<GrantedAuthority> getAuthorities() {
+    // TODO Auto-generated method stub
+    return null;
   }
 
-  @Override
-  @Column(nullable = false)
-  @XmlTransient
-  public String getPassword() {
-    return password;
+  @Column(name = "BRANCH_NAME", length = 20)
+  public String getBranchName() {
+    return branchName;
+  }
+
+  @Column(name = "CLIENT_ID", length = 20)
+  public String getClientId() {
+    return clientId;
+  }
+
+  @Column(name = "COMMENTS", length = 100)
+  public String getComments() {
+    return comments;
   }
 
   @Transient
@@ -101,22 +146,21 @@ public class User extends BaseObject implements Serializable, UserDetails {
     return confirmPassword;
   }
 
-  @Column(name = "password_hint")
-  @XmlTransient
-  public String getPasswordHint() {
-    return passwordHint;
+  @Temporal(TemporalType.DATE)
+  @Column(name = "CREATED_DATE", length = 7)
+  public Date getCreatedDate() {
+    return createdDate;
   }
 
-  @Column(name = "first_name", nullable = false, length = 50)
-  @SearchableProperty
-  public String getFirstName() {
-    return firstName;
+  @Column(name = "DATA_ID", precision = 10, scale = 0)
+  public Long getDataId() {
+    return dataId;
   }
 
-  @Column(name = "last_name", nullable = false, length = 50)
-  @SearchableProperty
-  public String getLastName() {
-    return lastName;
+  @Temporal(TemporalType.DATE)
+  @Column(name = "DISABLED_DATE", length = 7)
+  public Date getDisabledDate() {
+    return disabledDate;
   }
 
   @Column(nullable = false, unique = true)
@@ -125,15 +169,15 @@ public class User extends BaseObject implements Serializable, UserDetails {
     return email;
   }
 
-  @Column(name = "phone_number")
-  @SearchableProperty
-  public String getPhoneNumber() {
-    return phoneNumber;
+  @Column(name = "EMAIL_ADDRESS", length = 320)
+  public String getEmailAddress() {
+    return emailAddress;
   }
 
+  @Column(name = "first_name", nullable = false, length = 50)
   @SearchableProperty
-  public String getWebsite() {
-    return website;
+  public String getFirstName() {
+    return firstName;
   }
 
   /**
@@ -146,6 +190,92 @@ public class User extends BaseObject implements Serializable, UserDetails {
     return firstName + ' ' + lastName;
   }
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @SearchableId
+  public Long getId() {
+    return id;
+  }
+
+  @Temporal(TemporalType.DATE)
+  @Column(name = "LAST_LOGIN_DATE", length = 7)
+  public Date getLastLoginDate() {
+    return lastLoginDate;
+  }
+
+  @Column(name = "last_name", nullable = false, length = 50)
+  @SearchableProperty
+  public String getLastName() {
+    return lastName;
+  }
+
+  @Override
+  @Column(name = "PASSWORD", length = 150, nullable = false)
+  @XmlTransient
+  public String getPassword() {
+    return password;
+  }
+
+  @Temporal(TemporalType.DATE)
+  @Column(name = "PASSWORD_EXPIRY", length = 7)
+  public Date getPasswordExpiry() {
+    return passwordExpiry;
+  }
+
+  @Column(name = "password_hint")
+  @XmlTransient
+  public String getPasswordHint() {
+    return passwordHint;
+  }
+
+  @Column(name = "phone_number")
+  @SearchableProperty
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  @Column(name = "PW_SALT", length = 10)
+  public String getPwSalt() {
+    return pwSalt;
+  }
+
+  @Column(name = "ROLE_NAME", length = 20)
+  public String getRoleName() {
+    return roleName;
+  }
+
+  @Transient
+  public List<Role> getRolesList() {
+
+    final String[] stringList = roles.split(",");
+
+    final List<Role> rolesList = new ArrayList<Role>();
+
+    Long i = 0L;
+
+    for (final String roleName : stringList) {
+      final Role role = new Role();
+      role.setId(++i);
+      role.setName(roleName);
+
+      rolesList.add(role);
+    }
+
+    return rolesList;
+  }
+
+  @Override
+  @Column(name = "USERNAME", unique = true, length = 40, nullable = false)
+  @SearchableProperty
+  public String getUsername() {
+    return username;
+  }
+
+  @Column(name = "USER_ROLE_ID", precision = 10, scale = 0)
+  public Long getUserRoleId() {
+    return userRoleId;
+  }
+
   // @ManyToMany(fetch = FetchType.EAGER)
   // @JoinTable(name = "user_role", joinColumns = { @JoinColumn(name = "user_id") },
   // inverseJoinColumns = @JoinColumn(name = "role_id"))
@@ -154,15 +284,27 @@ public class User extends BaseObject implements Serializable, UserDetails {
     return version;
   }
 
+  @SearchableProperty
+  public String getWebsite() {
+    return website;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   @Override
-  @Column(name = "account_enabled")
-  public boolean isEnabled() {
-    return enabled;
+  public int hashCode() {
+    return (username != null ? username.hashCode() : 0);
   }
 
   @Column(name = "account_expired", nullable = false)
   public boolean isAccountExpired() {
     return accountExpired;
+  }
+
+  @Column(name = "account_locked", nullable = false)
+  public boolean isAccountLocked() {
+    return accountLocked;
   }
 
   /**
@@ -173,11 +315,6 @@ public class User extends BaseObject implements Serializable, UserDetails {
   @Transient
   public boolean isAccountNonExpired() {
     return !isAccountExpired();
-  }
-
-  @Column(name = "account_locked", nullable = false)
-  public boolean isAccountLocked() {
-    return accountLocked;
   }
 
   /**
@@ -205,52 +342,10 @@ public class User extends BaseObject implements Serializable, UserDetails {
     return !credentialsExpired;
   }
 
-  public void setId(final Long id) {
-    this.id = id;
-  }
-
-  public void setUsername(final String username) {
-    this.username = username;
-  }
-
-  public void setPassword(final String password) {
-    this.password = password;
-  }
-
-  public void setConfirmPassword(final String confirmPassword) {
-    this.confirmPassword = confirmPassword;
-  }
-
-  public void setPasswordHint(final String passwordHint) {
-    this.passwordHint = passwordHint;
-  }
-
-  public void setFirstName(final String firstName) {
-    this.firstName = firstName;
-  }
-
-  public void setLastName(final String lastName) {
-    this.lastName = lastName;
-  }
-
-  public void setEmail(final String email) {
-    this.email = email;
-  }
-
-  public void setPhoneNumber(final String phoneNumber) {
-    this.phoneNumber = phoneNumber;
-  }
-
-  public void setWebsite(final String website) {
-    this.website = website;
-  }
-
-  public void setVersion(final Integer version) {
-    this.version = version;
-  }
-
-  public void setEnabled(final boolean enabled) {
-    this.enabled = enabled;
+  @Override
+  @Column(name = "account_enabled")
+  public boolean isEnabled() {
+    return enabled;
   }
 
   public void setAccountExpired(final boolean accountExpired) {
@@ -261,34 +356,119 @@ public class User extends BaseObject implements Serializable, UserDetails {
     this.accountLocked = accountLocked;
   }
 
+  public void setAddress(final Address address) {
+    this.address = address;
+  }
+
+  public void setBranchName(final String branchName) {
+    this.branchName = branchName;
+  }
+
+  public void setClientId(final String clientId) {
+    this.clientId = clientId;
+  }
+
+  public void setComments(final String comments) {
+    this.comments = comments;
+  }
+
+  public void setConfirmPassword(final String confirmPassword) {
+    this.confirmPassword = confirmPassword;
+  }
+
+  public void setCreatedDate(final Date createdDate) {
+    this.createdDate = createdDate;
+  }
+
   public void setCredentialsExpired(final boolean credentialsExpired) {
     this.credentialsExpired = credentialsExpired;
   }
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public boolean equals(final Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (!(o instanceof User)) {
-      return false;
-    }
-
-    final User user = (User) o;
-
-    return !(username != null ? !username.equals(user.getUsername()) : user.getUsername() != null);
-
+  public void setDataId(final Long dataId) {
+    this.dataId = dataId;
   }
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public int hashCode() {
-    return (username != null ? username.hashCode() : 0);
+  public void setDisabledDate(final Date disabledDate) {
+    this.disabledDate = disabledDate;
+  }
+
+  public void setEmail(final String email) {
+    this.email = email;
+  }
+
+  public void setEmailAddress(final String emailAddress) {
+    this.emailAddress = emailAddress;
+  }
+
+  public void setEnabled(final boolean enabled) {
+    this.enabled = enabled;
+  }
+
+  public void setFirstName(final String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setId(final Long id) {
+    this.id = id;
+  }
+
+  public void setLastLoginDate(final Date lastLoginDate) {
+    this.lastLoginDate = lastLoginDate;
+  }
+
+  public void setLastName(final String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setPassword(final String password) {
+    this.password = password;
+  }
+
+  public void setPasswordExpiry(final Date passwordExpiry) {
+    this.passwordExpiry = passwordExpiry;
+  }
+
+  public void setPasswordHint(final String passwordHint) {
+    this.passwordHint = passwordHint;
+  }
+
+  public void setPhoneNumber(final String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public void setPwSalt(final String pwSalt) {
+    this.pwSalt = pwSalt;
+  }
+
+  public void setRoleName(final String roleName) {
+    this.roleName = roleName;
+  }
+
+  public void setRoles(final List<Role> roles) {
+
+    final StringBuffer rolesStringBuffer = new StringBuffer();
+
+    for (final Role role : roles) {
+      rolesStringBuffer.append(role.getName());
+    }
+
+    this.roles = rolesStringBuffer.toString();
+  }
+
+  public void setUsername(final String username) {
+    this.username = username;
+  }
+
+  public void setUserRoleId(final Long userRoleId) {
+    this.userRoleId = userRoleId;
+  }
+
+  public void setVersion(final Integer version) {
+    this.version = version;
+  }
+
+  public void setWebsite(final String website) {
+    this.website = website;
   }
 
   /**
@@ -317,56 +497,5 @@ public class User extends BaseObject implements Serializable, UserDetails {
       sb.append("No Granted Authorities");
     }
     return sb.toString();
-  }
-
-  @Override
-  @Transient
-  public Collection<GrantedAuthority> getAuthorities() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  public Address getAddress() {
-    return address;
-  }
-
-  public void setAddress(final Address address) {
-    this.address = address;
-  }
-
-  @Transient
-  public List<Role> getRolesList() {
-
-    final String[] stringList = roles.split(",");
-
-    final List<Role> rolesList = new ArrayList<Role>();
-
-    Long i = 0L;
-
-    for (final String roleName : stringList) {
-      final Role role = new Role();
-      role.setId(++i);
-      role.setName(roleName);
-
-      rolesList.add(role);
-    }
-
-    return rolesList;
-  }
-
-  public void setRoles(final List<Role> roles) {
-
-    final StringBuffer rolesStringBuffer = new StringBuffer();
-
-    for (final Role role : roles) {
-      rolesStringBuffer.append(role.getName());
-    }
-
-    this.roles = rolesStringBuffer.toString();
-  }
-
-  public void addRole(final Role role) {
-
-    roles += role.getName();
   }
 }
