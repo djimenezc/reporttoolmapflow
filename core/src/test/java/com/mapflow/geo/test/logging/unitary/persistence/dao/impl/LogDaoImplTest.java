e43488830d96c1beb2ccace25d26a5eab613977d
david.jimenez19 <david.jimenez19@gmail.com>
2011-12-15 15:48:43 +0000

    Added the test module
    Signed-off-by: david.jimenez19 <david.jimenez19@gmail.com>

--- core/src/test/java/com/mapflow/geo/test/logging/unitary/persistence/dao/impl/LogDaoImplTest.java
+++ core/src/test/java/com/mapflow/geo/test/logging/unitary/persistence/dao/impl/LogDaoImplTest.java
@@ -1,36 +1,31 @@
 package com.mapflow.geo.test.logging.unitary.persistence.dao.impl;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import java.util.Date;
 
-import junit.framework.TestCase;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.mapflow.geo.common.exceptions.MapflowAppException;
-import com.mapflow.geo.common.helper.dataaccess.DBHelper;
 import com.mapflow.geo.logging.dao.LogCountPointsDao;
 import com.mapflow.geo.logging.dao.LogMapdisplayDao;
-import com.mapflow.geo.logging.dao.impl.LogCountDaoImpl;
-import com.mapflow.geo.logging.dao.impl.LogMapdisplayDaoImpl;
 import com.mapflow.geo.logging.model.LogCounterFeaturesTo;
 import com.mapflow.geo.logging.model.LogMapdisplayTo;
 import com.mapflow.geo.logging.types.FeaturesCategoryType;
 
-public class LogDaoImplTest extends TestCase {
+public class LogDaoImplTest {
 
   private static final String TRANSACTION_ID = "46180";
 
+  @Autowired
   private LogMapdisplayDao logMapdisplayDao;
 
+  @Autowired
   private LogCountPointsDao logCountPointsDao;
 
   private static final String SERVICE_NAME_EXPECTED = null;
-
-  private static final String URL = "jdbc:oracle:thin:@192.168.160.108:1521/GEOSUAT.MAPFLOW.LOCAL";
-
-  private static final String PASSWORD = "Geoueu1ns";
-
-  private static final String USER = "GEOUEU";
-
-  private static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
 
   private LogCounterFeaturesTo buildLogCountFeaturesExample() throws MapflowAppException {
 
@@ -83,20 +78,7 @@ public class LogDaoImplTest extends TestCase {
     return log;
   }
 
-  @Override
-  protected void setUp() throws Exception {
-
-    final DBHelper dbhelper = new DBHelper();
-
-    logMapdisplayDao =
-      new LogMapdisplayDaoImpl(LogMapdisplayTo.class, DRIVER, USER, PASSWORD, URL, dbhelper);
-
-    logCountPointsDao =
-      new LogCountDaoImpl(LogCounterFeaturesTo.class, DRIVER, USER, PASSWORD, URL, dbhelper);
-
-    super.setUp();
-  }
-
+  @Test
   public void testRetrieveLogRowFromDatabase() {
 
     final LogMapdisplayTo log = logMapdisplayDao.get(TRANSACTION_ID);
@@ -109,6 +91,7 @@ public class LogDaoImplTest extends TestCase {
    * 
    * @throws MapflowAppException
    */
+  @Test
   public void testSaveLogCountFeatureRowInDatabase() throws MapflowAppException {
 
     LogCounterFeaturesTo log = buildLogCountFeaturesExample();
---
+++ test/src/test/java/com/mapflow/test/util/DataBaseUtils.java
@@ -0,0 +1,121 @@
+package com.mapflow.test.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class DataBaseUtils {
+
+  private final transient Log log = LogFactory.getLog(getClass());
+
+  private final String password;
+  private final String userName;
+  private final String dbDriver;
+  private final String dbUrl;
+
+  /**
+   * @param dbUrl
+   * @param dbDriver
+   * @param userName
+   * @param password
+   */
+  public DataBaseUtils(final String dbUrl, final String dbDriver,
+    final String userName, final String password) {
+    this.dbUrl = dbUrl;
+    this.dbDriver = dbDriver;
+    this.userName = userName;
+    this.password = password;
+  }
+
+  public final boolean executeQuery(final String query) throws SQLException {
+
+    Statement sentencias = null;
+
+    final Connection con = getConnection();
+
+    try {
+
+      sentencias = con.createStatement();
+      sentencias.executeUpdate(query);
+    }
+    catch (final Exception e) {
+
+      log.error(e.getMessage());
+
+      return false;
+    }
+    finally {
+
+      sentencias.close();
+      con.close();
+    }
+
+    return true;
+
+  }
+
+  public final Connection getConnection() throws SQLException {
+
+    try {
+      Class.forName(dbDriver).newInstance();
+    }
+    catch (final ClassNotFoundException cnfe) {
+
+      log.error(cnfe.getMessage());
+      return null;
+    }
+    catch (final InstantiationException ie) {
+
+      log.error(ie.getMessage());
+      return null;
+    }
+    catch (final IllegalAccessException iae) {
+
+      log.error(iae.getMessage());
+      return null;
+    }
+
+    try {
+      final Connection con =
+        DriverManager.getConnection(this.dbUrl, userName, password);
+      return con;
+    }
+    catch (final SQLException sqle) {
+
+      log.error(sqle.getMessage());
+
+      return null;
+    }
+  }
+
+  public final boolean resetSchema(final String schemaName) throws SQLException {
+
+    Statement sentencias = null;
+
+    final Connection con = getConnection();
+
+    try {
+      sentencias = con.createStatement();
+      sentencias.executeUpdate("DROP SCHEMA " + schemaName);
+      sentencias.executeUpdate("CREATE SCHEMA " + schemaName);
+    }
+    catch (final Exception e) {
+
+      log.error(e.getMessage());
+
+      return false;
+    }
+    finally {
+
+      sentencias.close();
+      con.close();
+    }
+
+    return true;
+  }
+
+}
