4bdf456f67de843b488ec782e15d7a6aebfdae57
david.jimenez19 <david.jimenez19@gmail.com>
2011-12-16 17:12:02 +0000

    Change log level Adjust test
    Signed-off-by: david.jimenez19 <david.jimenez19@gmail.com>

--- core/src/test/java/org/appfuse/dao/BaseDaoTestCase.java
+++ core/src/test/java/org/appfuse/dao/BaseDaoTestCase.java
@@ -1,5 +1,11 @@
 package org.appfuse.dao;
 
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -10,83 +16,78 @@ import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
-
 /**
  * Base class for running DAO tests.
- *
+ * 
  * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
  */
-@ContextConfiguration(
-        locations = {"classpath:/applicationContext-resources.xml",
-                "classpath:/applicationContext-dao.xml",
-                "classpath*:/applicationContext.xml",
-                "classpath:**/applicationContext*.xml"})
+@ContextConfiguration(locations = { "classpath:/spring/applicationContext-resources.xml",
+  "classpath:/spring/applicationContext-dao.xml", "classpath*:/spring/applicationContext.xml",
+  "classpath:**/spring/applicationContext*.xml" })
 public abstract class BaseDaoTestCase extends AbstractTransactionalJUnit4SpringContextTests {
-    @Autowired
-    private SessionFactory sessionFactory;
 
-    /**
-     * Log variable for all child classes. Uses LogFactory.getLog(getClass()) from Commons Logging
-     */
-    protected final Log log = LogFactory.getLog(getClass());
-    /**
-     * ResourceBundle loaded from src/test/resources/${package.name}/ClassName.properties (if exists)
-     */
-    protected ResourceBundle rb;
+  @Autowired
+  private SessionFactory sessionFactory;
 
-    /**
-     * Default constructor - populates "rb" variable if properties file exists for the class in
-     * src/test/resources.
-     */
-    public BaseDaoTestCase() {
-        // Since a ResourceBundle is not required for each class, just
-        // do a simple check to see if one exists
-        String className = this.getClass().getName();
+  /**
+   * Log variable for all child classes. Uses LogFactory.getLog(getClass()) from Commons Logging
+   */
+  protected final Log log = LogFactory.getLog(getClass());
+  /**
+   * ResourceBundle loaded from src/test/resources/${package.name}/ClassName.properties (if exists)
+   */
+  protected ResourceBundle rb;
 
-        try {
-            rb = ResourceBundle.getBundle(className);
-        } catch (MissingResourceException mre) {
-            log.trace("No resource bundle found for: " + className);
-        }
+  /**
+   * Default constructor - populates "rb" variable if properties file exists for the class in
+   * src/test/resources.
+   */
+  public BaseDaoTestCase() {
+    // Since a ResourceBundle is not required for each class, just
+    // do a simple check to see if one exists
+    final String className = this.getClass().getName();
+
+    try {
+      rb = ResourceBundle.getBundle(className);
+    }
+    catch (final MissingResourceException mre) {
+      log.trace("No resource bundle found for: " + className);
+    }
+  }
+
+  /**
+   * Create a HibernateTemplate from the SessionFactory and call flush() and clear() on it. Designed
+   * to be used after "save" methods in tests: http://issues.appfuse.org/browse/APF-178.
+   * 
+   * @throws org.springframework.beans.BeansException
+   *           when can't find 'sessionFactory' bean
+   */
+  protected void flush() throws BeansException {
+    final HibernateTemplate hibernateTemplate = new HibernateTemplate(sessionFactory);
+    hibernateTemplate.flush();
+    hibernateTemplate.clear();
+  }
+
+  /**
+   * Utility method to populate a javabean-style object with values from a Properties file
+   * 
+   * @param obj
+   *          the model object to populate
+   * @return Object populated object
+   * @throws Exception
+   *           if BeanUtils fails to copy properly
+   */
+  protected Object populate(final Object obj) throws Exception {
+    // loop through all the beans methods and set its properties from its .properties file
+    final Map<String, String> map = new HashMap<String, String>();
+
+    for (final Enumeration<String> keys = rb.getKeys(); keys.hasMoreElements();) {
+      final String key = keys.nextElement();
+      map.put(key, rb.getString(key));
     }
 
-    /**
-     * Utility method to populate a javabean-style object with values
-     * from a Properties file
-     *
-     * @param obj the model object to populate
-     * @return Object populated object
-     * @throws Exception if BeanUtils fails to copy properly
-     */
-    protected Object populate(final Object obj) throws Exception {
-        // loop through all the beans methods and set its properties from its .properties file
-        Map<String, String> map = new HashMap<String, String>();
+    BeanUtils.copyProperties(obj, map);
 
-        for (Enumeration<String> keys = rb.getKeys(); keys.hasMoreElements();) {
-            String key = keys.nextElement();
-            map.put(key, rb.getString(key));
-        }
-
-        BeanUtils.copyProperties(obj, map);
-
-        return obj;
-    }
-
-    /**
-     * Create a HibernateTemplate from the SessionFactory and call flush() and clear() on it.
-     * Designed to be used after "save" methods in tests: http://issues.appfuse.org/browse/APF-178.
-     *
-     * @throws org.springframework.beans.BeansException
-     *          when can't find 'sessionFactory' bean
-     */
-    protected void flush() throws BeansException {
-        HibernateTemplate hibernateTemplate = new HibernateTemplate(sessionFactory);
-        hibernateTemplate.flush();
-        hibernateTemplate.clear();
-    }
+    return obj;
+  }
 }
