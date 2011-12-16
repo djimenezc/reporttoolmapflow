499ee087b8cad733927855f1d4544910e7015f81
david.jimenez19 <david.jimenez19@gmail.com>
2011-12-16 10:14:35 +0000

    the app start with tomcat
    Signed-off-by: david.jimenez19 <david.jimenez19@gmail.com>

--- web/src/test/java/com/mapflow/webapp/controller/BaseControllerTestCase.java
+++ web/src/test/java/com/mapflow/webapp/controller/BaseControllerTestCase.java
@@ -8,42 +8,42 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
-@ContextConfiguration(
-        locations = {"classpath:/applicationContext-resources.xml",
-                "classpath:/applicationContext-dao.xml",
-                "classpath:/applicationContext-service.xml",
-                "classpath*:/applicationContext.xml", // for modular archetypes
-                "/WEB-INF/applicationContext*.xml",
-                "/WEB-INF/dispatcher-servlet.xml"})
+@ContextConfiguration(locations = { "classpath:/spring/applicationContext-resources.xml",
+  "classpath:/spring/applicationContext-dao.xml",
+  "classpath:/spring/applicationContext-service.xml", "classpath*:/spring/applicationContext.xml",
+  "/WEB-INF/spring/applicationContext*.xml", "/WEB-INF/spring/dispatcher-servlet.xml" })
 public abstract class BaseControllerTestCase extends AbstractTransactionalJUnit4SpringContextTests {
-    protected transient final Log log = LogFactory.getLog(getClass());
-    private int smtpPort = 25250;
 
-    @Before
-    public void onSetUp() {
-        smtpPort = smtpPort + (int) (Math.random() * 100);
-        // change the port on the mailSender so it doesn't conflict with an
-        // existing SMTP server on localhost
-        JavaMailSenderImpl mailSender = (JavaMailSenderImpl) applicationContext.getBean("mailSender");
-        mailSender.setPort(getSmtpPort());
-        mailSender.setHost("localhost");
-    }
+  protected transient final Log log = LogFactory.getLog(getClass());
+  private int smtpPort = 25250;
 
-    protected int getSmtpPort() {
-        return smtpPort;
-    }
+  protected int getSmtpPort() {
+    return smtpPort;
+  }
 
-    /**
-     * Convenience methods to make tests simpler
-     *
-     * @param url the URL to post to
-     * @return a MockHttpServletRequest with a POST to the specified URL
-     */
-    public MockHttpServletRequest newPost(String url) {
-        return new MockHttpServletRequest("POST", url);
-    }
+  public MockHttpServletRequest newGet(final String url) {
+    return new MockHttpServletRequest("GET", url);
+  }
 
-    public MockHttpServletRequest newGet(String url) {
-        return new MockHttpServletRequest("GET", url);
-    }
+  /**
+   * Convenience methods to make tests simpler
+   * 
+   * @param url
+   *          the URL to post to
+   * @return a MockHttpServletRequest with a POST to the specified URL
+   */
+  public MockHttpServletRequest newPost(final String url) {
+    return new MockHttpServletRequest("POST", url);
+  }
+
+  @Before
+  public void onSetUp() {
+    smtpPort = smtpPort + (int) (Math.random() * 100);
+    // change the port on the mailSender so it doesn't conflict with an
+    // existing SMTP server on localhost
+    final JavaMailSenderImpl mailSender =
+      (JavaMailSenderImpl) applicationContext.getBean("mailSender");
+    mailSender.setPort(getSmtpPort());
+    mailSender.setHost("localhost");
+  }
 }
