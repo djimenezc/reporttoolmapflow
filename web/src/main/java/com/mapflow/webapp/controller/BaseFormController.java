c071716856e40ffd3df7f2e23451a01ca24e28ad
david.jimenez19 <david.jimenez19@gmail.com>
2012-02-17 12:19:07 +0000

    Fixed problem with the resources languages file, there were a wrong path

--- web/src/main/java/com/mapflow/webapp/controller/BaseFormController.java
+++ web/src/main/java/com/mapflow/webapp/controller/BaseFormController.java
@@ -32,9 +32,10 @@ import com.mapflow.geo.common.model.entities.User;
 import com.mapflow.geo.common.service.MailEngine;
 
 /**
- * Implementation of <strong>SimpleFormController</strong> that contains convenience methods for
- * subclasses. For example, getting the current user and saving messages/errors. This class is
- * intended to be a base class for all Form controllers.
+ * Implementation of <strong>SimpleFormController</strong> that contains
+ * convenience methods for subclasses. For example, getting the current user and
+ * saving messages/errors. This class is intended to be a base class for all
+ * Form controllers.
  * <p>
  * <a href="BaseFormController.java.html"><i>View Source</i></a>
  * </p>
@@ -43,205 +44,227 @@ import com.mapflow.geo.common.service.MailEngine;
  */
 public class BaseFormController implements ServletContextAware {
 
-  protected final transient Log log = LogFactory.getLog(getClass());
-  public static final String MESSAGES_KEY = "successMessages";
-  private UserManager userManager = null;
-  protected MailEngine mailEngine = null;
-  protected SimpleMailMessage message = null;
-  protected String templateName = "accountCreated.vm";
-  protected String cancelView;
-  protected String successView;
+	protected final transient Log log = LogFactory.getLog(getClass());
+	public static final String MESSAGES_KEY = "successMessages";
+	private UserManager userManager = null;
+	protected MailEngine mailEngine = null;
+	protected SimpleMailMessage message = null;
+	protected String templateName = "accountCreated.vm";
+	protected String cancelView;
+	protected String successView;
 
-  private MessageSourceAccessor messages;
-  private ServletContext servletContext;
+	private MessageSourceAccessor messages;
+	private ServletContext servletContext;
 
-  @Autowired(required = false)
-  Validator validator;
+	@Autowired(required = false)
+	Validator validator;
 
-  @Autowired
-  public void setMessages(final MessageSource messageSource) {
-    messages = new MessageSourceAccessor(messageSource);
-  }
+	public final String getCancelView() {
+		// Default to successView if cancelView is invalid
+		if ((cancelView == null) || (cancelView.length() == 0)) {
+			return getSuccessView();
+		}
+		return cancelView;
+	}
 
-  @Autowired
-  public void setUserManager(final UserManager userManager) {
-    this.userManager = userManager;
-  }
+	/**
+	 * Convenience method to get the Configuration HashMap from the servlet
+	 * context.
+	 * 
+	 * @return the user's populated form from the session
+	 */
+	@SuppressWarnings("rawtypes")
+	public Map<?, ?> getConfiguration() {
+		final Map<?, ?> config = (HashMap<?, ?>) servletContext
+				.getAttribute(Constants.CONFIG);
 
-  public UserManager getUserManager() {
-    return userManager;
-  }
+		// so unit tests don't puke when nothing's been set
+		if (config == null) {
+			return new HashMap();
+		}
 
-  @SuppressWarnings("unchecked")
-  public void saveError(final HttpServletRequest request, final String error) {
-    List<String> errors = (List<String>) request.getSession().getAttribute("errors");
-    if (errors == null) {
-      errors = new ArrayList<String>();
-    }
-    errors.add(error);
-    request.getSession().setAttribute("errors", errors);
-  }
+		return config;
+	}
 
-  @SuppressWarnings("unchecked")
-  public void saveMessage(final HttpServletRequest request, final String msg) {
-    List<String> messages = (List<String>) request.getSession().getAttribute(MESSAGES_KEY);
+	protected ServletContext getServletContext() {
+		return servletContext;
+	}
 
-    if (messages == null) {
-      messages = new ArrayList<String>();
-    }
+	public final String getSuccessView() {
+		return successView;
+	}
 
-    messages.add(msg);
-    request.getSession().setAttribute(MESSAGES_KEY, messages);
-  }
+	/**
+	 * Convenience method for getting a i18n key's value. Calling
+	 * getMessageSourceAccessor() is used because the RequestContext variable is
+	 * not set in unit tests b/c there's no DispatchServlet Request.
+	 * 
+	 * @param msgKey
+	 * @param locale
+	 *            the current locale
+	 * @return
+	 */
+	public String getText(final String msgKey, final Locale locale) {
+		return messages.getMessage(msgKey, locale);
+	}
 
-  /**
-   * Convenience method for getting a i18n key's value. Calling getMessageSourceAccessor() is used
-   * because the RequestContext variable is not set in unit tests b/c there's no DispatchServlet
-   * Request.
-   * 
-   * @param msgKey
-   * @param locale
-   *          the current locale
-   * @return
-   */
-  public String getText(final String msgKey, final Locale locale) {
-    return messages.getMessage(msgKey, locale);
-  }
+	/**
+	 * Convenience method for getting a i18n key's value with arguments.
+	 * 
+	 * @param msgKey
+	 * @param args
+	 * @param locale
+	 *            the current locale
+	 * @return
+	 */
+	public String getText(final String msgKey, final Object[] args,
+			final Locale locale) {
+		return messages.getMessage(msgKey, args, locale);
+	}
 
-  /**
-   * Convenient method for getting a i18n key's value with a single string argument.
-   * 
-   * @param msgKey
-   * @param arg
-   * @param locale
-   *          the current locale
-   * @return
-   */
-  public String getText(final String msgKey, final String arg, final Locale locale) {
-    return getText(msgKey, new Object[] { arg }, locale);
-  }
+	/**
+	 * Convenient method for getting a i18n key's value with a single string
+	 * argument.
+	 * 
+	 * @param msgKey
+	 * @param arg
+	 * @param locale
+	 *            the current locale
+	 * @return
+	 */
+	public String getText(final String msgKey, final String arg,
+			final Locale locale) {
+		return getText(msgKey, new Object[] { arg }, locale);
+	}
 
-  /**
-   * Convenience method for getting a i18n key's value with arguments.
-   * 
-   * @param msgKey
-   * @param args
-   * @param locale
-   *          the current locale
-   * @return
-   */
-  public String getText(final String msgKey, final Object[] args, final Locale locale) {
-    return messages.getMessage(msgKey, args, locale);
-  }
+	public UserManager getUserManager() {
+		return userManager;
+	}
 
-  /**
-   * Convenience method to get the Configuration HashMap from the servlet context.
-   * 
-   * @return the user's populated form from the session
-   */
-  @SuppressWarnings("rawtypes")
-  public Map<?, ?> getConfiguration() {
-    final Map<?, ?> config = (HashMap<?, ?>) servletContext.getAttribute(Constants.CONFIG);
+	/**
+	 * Set up a custom property editor for converting form inputs to real
+	 * objects
+	 * 
+	 * @param request
+	 *            the current request
+	 * @param binder
+	 *            the data binder
+	 */
+	@InitBinder
+	protected void initBinder(final HttpServletRequest request,
+			final ServletRequestDataBinder binder) {
+		binder.registerCustomEditor(Integer.class, null,
+				new CustomNumberEditor(Integer.class, null, true));
+		binder.registerCustomEditor(Long.class, null, new CustomNumberEditor(
+				Long.class, null, true));
+		binder.registerCustomEditor(byte[].class,
+				new ByteArrayMultipartFileEditor());
 
-    // so unit tests don't puke when nothing's been set
-    if (config == null) {
-      return new HashMap();
-    }
+		SimpleDateFormat dateFormat;
 
-    return config;
-  }
+		try {
+			dateFormat = new SimpleDateFormat(getText("date.format",
+					request.getLocale()));
+		} catch (final Exception e) {
 
-  /**
-   * Set up a custom property editor for converting form inputs to real objects
-   * 
-   * @param request
-   *          the current request
-   * @param binder
-   *          the data binder
-   */
-  @InitBinder
-  protected void initBinder(final HttpServletRequest request, final ServletRequestDataBinder binder) {
-    binder.registerCustomEditor(Integer.class, null, new CustomNumberEditor(Integer.class, null,
-      true));
-    binder.registerCustomEditor(Long.class, null, new CustomNumberEditor(Long.class, null, true));
-    binder.registerCustomEditor(byte[].class, new ByteArrayMultipartFileEditor());
-    final SimpleDateFormat dateFormat =
-      new SimpleDateFormat(getText("date.format", request.getLocale()));
-    dateFormat.setLenient(false);
-    binder.registerCustomEditor(Date.class, null, new CustomDateEditor(dateFormat, true));
-  }
+			dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		}
+		dateFormat.setLenient(false);
+		binder.registerCustomEditor(Date.class, null, new CustomDateEditor(
+				dateFormat, true));
+	}
 
-  /**
-   * Convenience message to send messages to users, includes app URL as footer.
-   * 
-   * @param user
-   *          the user to send a message to.
-   * @param msg
-   *          the message to send.
-   * @param url
-   *          the URL of the application.
-   */
-  protected void sendUserMessage(final User user, final String msg, final String url) {
-    if (log.isDebugEnabled()) {
-      log.debug("sending e-mail to user [" + user.getEmail() + "]...");
-    }
+	@SuppressWarnings("unchecked")
+	public void saveError(final HttpServletRequest request, final String error) {
+		List<String> errors = (List<String>) request.getSession().getAttribute(
+				"errors");
+		if (errors == null) {
+			errors = new ArrayList<String>();
+		}
+		errors.add(error);
+		request.getSession().setAttribute("errors", errors);
+	}
 
-    message.setTo(user.getFullName() + "<" + user.getEmail() + ">");
+	@SuppressWarnings("unchecked")
+	public void saveMessage(final HttpServletRequest request, final String msg) {
+		List<String> messages = (List<String>) request.getSession()
+				.getAttribute(MESSAGES_KEY);
 
-    final Map<String, Serializable> model = new HashMap<String, Serializable>();
-    model.put("user", user);
+		if (messages == null) {
+			messages = new ArrayList<String>();
+		}
 
-    // TODO: once you figure out how to get the global resource bundle in
-    // WebWork, then figure it out here too. In the meantime, the Username
-    // and Password labels are hard-coded into the template.
-    // model.put("bundle", getTexts());
-    model.put("message", msg);
-    model.put("applicationURL", url);
-    mailEngine.sendMessage(message, templateName, model);
-  }
+		messages.add(msg);
+		request.getSession().setAttribute(MESSAGES_KEY, messages);
+	}
 
-  @Autowired
-  public void setMailEngine(final MailEngine mailEngine) {
-    this.mailEngine = mailEngine;
-  }
+	/**
+	 * Convenience message to send messages to users, includes app URL as
+	 * footer.
+	 * 
+	 * @param user
+	 *            the user to send a message to.
+	 * @param msg
+	 *            the message to send.
+	 * @param url
+	 *            the URL of the application.
+	 */
+	protected void sendUserMessage(final User user, final String msg,
+			final String url) {
+		if (log.isDebugEnabled()) {
+			log.debug("sending e-mail to user [" + user.getEmail() + "]...");
+		}
 
-  @Autowired
-  public void setMessage(final SimpleMailMessage message) {
-    this.message = message;
-  }
+		message.setTo(user.getFullName() + "<" + user.getEmail() + ">");
 
-  public void setTemplateName(final String templateName) {
-    this.templateName = templateName;
-  }
+		final Map<String, Serializable> model = new HashMap<String, Serializable>();
+		model.put("user", user);
 
-  public final BaseFormController setCancelView(final String cancelView) {
-    this.cancelView = cancelView;
-    return this;
-  }
+		// TODO: once you figure out how to get the global resource bundle in
+		// WebWork, then figure it out here too. In the meantime, the Username
+		// and Password labels are hard-coded into the template.
+		// model.put("bundle", getTexts());
+		model.put("message", msg);
+		model.put("applicationURL", url);
+		mailEngine.sendMessage(message, templateName, model);
+	}
 
-  public final String getCancelView() {
-    // Default to successView if cancelView is invalid
-    if ((cancelView == null) || (cancelView.length() == 0)) {
-      return getSuccessView();
-    }
-    return cancelView;
-  }
+	public final BaseFormController setCancelView(final String cancelView) {
+		this.cancelView = cancelView;
+		return this;
+	}
 
-  public final String getSuccessView() {
-    return successView;
-  }
+	@Autowired
+	public void setMailEngine(final MailEngine mailEngine) {
+		this.mailEngine = mailEngine;
+	}
 
-  public final BaseFormController setSuccessView(final String successView) {
-    this.successView = successView;
-    return this;
-  }
+	@Autowired
+	public void setMessage(final SimpleMailMessage message) {
+		this.message = message;
+	}
 
-  @Override
-  public void setServletContext(final ServletContext servletContext) {
-    this.servletContext = servletContext;
-  }
+	@Autowired
+	public void setMessages(final MessageSource messageSource) {
+		messages = new MessageSourceAccessor(messageSource);
+	}
 
-  protected ServletContext getServletContext() {
-    return servletContext;
-  }
+	@Override
+	public void setServletContext(final ServletContext servletContext) {
+		this.servletContext = servletContext;
+	}
+
+	public final BaseFormController setSuccessView(final String successView) {
+		this.successView = successView;
+		return this;
+	}
+
+	public void setTemplateName(final String templateName) {
+		this.templateName = templateName;
+	}
+
+	@Autowired
+	public void setUserManager(final UserManager userManager) {
+		this.userManager = userManager;
+	}
 }
